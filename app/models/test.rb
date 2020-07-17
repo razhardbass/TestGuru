281ddf2aa40d32dd77f644e6df44861b20436f9a
Misha <mian_12@mail.ru>
2020-07-18 00:08:55 +0300

    lesson4 completed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category)
+    Test.joins('INNER JOIN categories ON categories.id = tests.category_id').where("categories.title = :category", category: category).order(id: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  def level_tests(level)
+    Test.joins('INNER JOIN categories ON tests.category_id = categories.id ').where(level: level).pluck(:title)
+  end
 end
