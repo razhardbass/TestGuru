e430e8bd1a5ff72021a20b49a68614f9ba5bf8ba
Misha <mian_12@mail.ru>
2020-07-30 22:21:20 +0300

    fixed methods in controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: %i[index]
-  before_action :find_question, only: %i[show find destroy]
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -13,6 +13,23 @@ class QuestionsController < ApplicationController
     render plain: @question.body
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = @test.questions.create(question_params)
+    @question.save
+    redirect_to @test
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to @test 
+  end
+
+  private
+
   def find_test
     @test = Test.find(params[:test_id])
   end
@@ -21,28 +38,13 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
-  def new
-    
-  end
-
-  def create
-    question = Question.create(question_params)
-
-    render plain: question.inspect
-  end
-
-  def destroy
-    @question.destroy
-    redirect_to @question.test
+  def question_params
+    params.require(:question).permit(:test_id, :body)
   end
 
   def rescue_with_question_not_found
     render plain: 'Question was not found'
   end
 
-  private
-
-  def question_params
-    params.require(:question).permit(:body, :test_id)
-  end
 end
+
